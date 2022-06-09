1f63e3d3f1b9958d86fc6f79e9c5b519edd765d2
abel-baker <seth.keating@gmail.com>
2022-06-09 16:31:27 -0400

    handlebars setup

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const session = require('express-session');
+const path = require('path');
+
+const expressHandlebars = require('express-handlebars');
+const handlebars = expressHandlebars.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,8 +23,13 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
 
